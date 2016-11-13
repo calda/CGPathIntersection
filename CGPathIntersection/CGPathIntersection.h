3be0370051e8e3d5ad441cd547f2876322e14b8d
Cal Stephens <cal@calstephens.tech>
2016-11-13 12:04:10 -0500

    Initial Commit

---
+++ CGPathIntersection/CGPathIntersection.h
@@ -0,0 +1,19 @@
+//
+//  CGPathIntersection.h
+//  CGPathIntersection
+//
+//  Created by Cal Stephens on 11/13/16.
+//  Copyright © 2016 Cal Stephens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGPathIntersection.
+FOUNDATION_EXPORT double CGPathIntersectionVersionNumber;
+
+//! Project version string for CGPathIntersection.
+FOUNDATION_EXPORT const unsigned char CGPathIntersectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGPathIntersection/PublicHeader.h>
+
+
